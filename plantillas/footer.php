ba4a2a35f6e7667364fc29c954013c4de18be395
RangerLeonardo <brandonleonardobarrera@gmail.com>
2025-05-29 17:09:34 -0600

    Adaptado para github pages

--- plantillas/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-        <p class="footer__texto">&copy;Todos los derechos reservados 2022</p>
-</footer>
--- plantillas/logo.php
+++
@@ -1,3 +0,0 @@
-<header class="header">
-        <a href="/index.php"><img src="/img/logo.png" alt="Logo cuidado con el perreo" class="header__logo"></a>
-</header>
